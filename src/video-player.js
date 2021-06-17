ae3e9914f3e0d05d794aa212cf96daaa952fc6b0
Jeroen Carolus <j.carolus@peoplesplayground.nl>
2021-06-17 15:01:43 +0200

    * README for Chromecast. lint the video-player

--- src/video-player.js
+++ src/video-player.js
@@ -1,16 +1,16 @@
-import EmbedPlayer from "./embed-player.js";
+import EmbedPlayer from './embed-player.js';
 
 (function () {
     const urlQueryString = window.location.search;
     const urlParams = new URLSearchParams(urlQueryString);
 
-    const articleId = +urlParams.get("articleId");
-    const projectId = +urlParams.get("projectId");
-    const assetId = +urlParams.get("assetId");
-    const apiBaseUrl = urlParams.get("apiBaseUrl");
-    const token = urlParams.get("token");
-    const posterImageUrl = urlParams.get("posterImageUrl");
-    const autoplay = urlParams.get("autoplay");
+    const articleId = +urlParams.get('articleId');
+    const projectId = +urlParams.get('projectId');
+    const assetId = +urlParams.get('assetId');
+    const apiBaseUrl = urlParams.get('apiBaseUrl');
+    const token = urlParams.get('token');
+    const posterImageUrl = urlParams.get('posterImageUrl');
+    const autoplay = urlParams.get('autoplay');
 
     const tokenParameter = token ? {token} : {};
     const posterImageUrlParameter = posterImageUrl ? {posterImageUrl} : {};
@@ -19,14 +19,14 @@ import EmbedPlayer from "./embed-player.js";
 
     player
         .play({
-            selector: ".video-wrapper",
+            selector: '.video-wrapper',
             apiBaseUrl,
             articleId,
             projectId,
             assetId,
             ...tokenParameter,
             ...posterImageUrlParameter,
-            autoplay: autoplay && autoplay === "true",
+            autoplay: autoplay && autoplay === 'true',
         })
         .catch((error) => {
             console.error(error);
