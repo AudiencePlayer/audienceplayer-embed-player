17def9b689df631e2973932fe0fd8eab4899eaa5
Jeroen Carolus <j.carolus@peoplesplayground.nl>
2021-06-17 14:01:00 +0200

    * add eslint and rules

--- src/embed-player.js
+++ src/embed-player.js
@@ -12,15 +12,15 @@ export default class EmbedPlayer {
     initPlayer(selector) {
         this.destroy();
         const videoContainer = document.querySelector(selector);
-        const videoElement = document.createElement("video");
+        const videoElement = document.createElement('video');
         videoElement.setAttribute(
-            "class",
-            ["azuremediaplayer", "amp-flush-skin", "amp-big-play-centered"].join(" ")
+            'class',
+            ['azuremediaplayer', 'amp-flush-skin', 'amp-big-play-centered'].join(' ')
         );
-        videoElement.setAttribute("tabIndex", "0");
-        videoElement.setAttribute("width", "100%");
-        videoElement.setAttribute("height", "100%");
-        videoElement.setAttribute("id", "azuremediaplayer");
+        videoElement.setAttribute('tabIndex', '0');
+        videoElement.setAttribute('width', '100%');
+        videoElement.setAttribute('height', '100%');
+        videoElement.setAttribute('id', 'azuremediaplayer');
         videoContainer.appendChild(videoElement);
     }
 
@@ -35,19 +35,19 @@ export default class EmbedPlayer {
              autoplay,
          }) {
         if (!selector) {
-            return Promise.reject("selector property is missing");
+            return Promise.reject('selector property is missing');
         }
         if (!apiBaseUrl) {
-            return Promise.reject("apiBaseUrl property is missing");
+            return Promise.reject('apiBaseUrl property is missing');
         }
         if (!articleId) {
-            return Promise.reject("articleId property is missing");
+            return Promise.reject('articleId property is missing');
         }
         if (!assetId) {
-            return Promise.reject("assetId property is missing");
+            return Promise.reject('assetId property is missing');
         }
         if (!projectId) {
-            return Promise.reject("projectId property is missing");
+            return Promise.reject('projectId property is missing');
         }
         const apiFetchUrl = `${apiBaseUrl}/graphql/${projectId}`;
         const heartBeatUrl = `${apiBaseUrl}/service/analytics/stream/pulse/`;
@@ -72,7 +72,7 @@ export default class EmbedPlayer {
     }
 
     playVideo(configData, posterUrl, autoplay) {
-        const videoElement = document.querySelector("video");
+        const videoElement = document.querySelector('video');
         var myOptions = {
             autoplay,
             controls: true,
@@ -88,23 +88,23 @@ export default class EmbedPlayer {
 
     bindEvents(configData) {
         if (this.myPlayer) {
-            this.myPlayer.addEventListener("error", (event) =>
+            this.myPlayer.addEventListener('error', (event) =>
                 this.eventHandler(event, configData)
             );
 
-            this.myPlayer.addEventListener("ended", (event) =>
+            this.myPlayer.addEventListener('ended', (event) =>
                 this.eventHandler(event, configData)
             );
 
-            this.myPlayer.addEventListener("pause", (event) =>
+            this.myPlayer.addEventListener('pause', (event) =>
                 this.eventHandler(event, configData)
             );
 
-            this.myPlayer.addEventListener("timeupdate", (event) =>
+            this.myPlayer.addEventListener('timeupdate', (event) =>
                 this.eventHandler(event, configData)
             );
 
-            this.myPlayer.addEventListener("playing", (event) =>
+            this.myPlayer.addEventListener('playing', (event) =>
                 this.eventHandler(event, configData)
             );
         }
@@ -112,11 +112,11 @@ export default class EmbedPlayer {
 
     eventHandler(event, config) {
         switch (event.type) {
-            case "timeupdate": {
+            case 'timeupdate': {
                 if (this.isPlaying) {
                     if (Date.now() - this.lastPlayTime > 30000) {
                         this.sendPulse(
-                            config.heartBeatUrl + "update",
+                            config.heartBeatUrl + 'update',
                             this.getHeartBeatParams(config)
                         );
                         this.lastPlayTime = Date.now();
@@ -124,31 +124,31 @@ export default class EmbedPlayer {
                 }
             }
                 break;
-            case "playing": {
+            case 'playing': {
                 if (this.isFirstPlay) {
                     this.isFirstPlay = false;
                     this.myPlayer.currentTime(config.currentTime);
                 }
                 this.sendPulse(
-                    config.heartBeatUrl + "init",
+                    config.heartBeatUrl + 'init',
                     this.getHeartBeatParams(config)
                 );
                 this.lastPlayTime = Date.now();
                 this.isPlaying = true;
             }
                 break;
-            case "pause": {
+            case 'pause': {
                 this.sendPulse(
-                    config.heartBeatUrl + "update",
+                    config.heartBeatUrl + 'update',
                     this.getHeartBeatParams(config)
                 );
                 this.lastPlayTime = Date.now();
                 this.isPlaying = false;
             }
                 break;
-            case "ended": {
+            case 'ended': {
                 this.sendPulse(
-                    config.heartBeatUrl + "finish",
+                    config.heartBeatUrl + 'finish',
                     this.getHeartBeatParams(config)
                 );
                 this.lastPlayTime = Date.now();
@@ -160,9 +160,9 @@ export default class EmbedPlayer {
 
     getHeartBeatParams(config) {
         return {
-            appa: "" + this.myPlayer.currentTime(),
+            appa: '' + this.myPlayer.currentTime(),
             appr:
-                "" +
+                '' +
                 Math.min(this.myPlayer.currentTime() / this.myPlayer.duration(), 1),
             pulseToken: config.pulseToken,
         };
@@ -231,13 +231,13 @@ export default class EmbedPlayer {
             }
         `;
 
-        const authHeader = token ? {Authorization: "Bearer " + token} : {};
+        const authHeader = token ? {Authorization: 'Bearer ' + token} : {};
 
         return fetch(apiBaseUrl, {
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
                 ...authHeader,
             },
             body: JSON.stringify({
@@ -273,18 +273,18 @@ export default class EmbedPlayer {
             }
         `;
 
-        const authHeader = token ? {Authorization: "Bearer " + token} : {};
+        const authHeader = token ? {Authorization: 'Bearer ' + token} : {};
 
         return fetch(apiBaseUrl, {
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
                 ...authHeader,
             },
             body: JSON.stringify({
                 query: articleAssetPlayMutation,
-                variables: {articleId, assetId, protocols: ["dash", "mss", "hls"]},
+                variables: {articleId, assetId, protocols: ['dash', 'mss', 'hls']},
             }),
         });
     }
@@ -294,7 +294,7 @@ export default class EmbedPlayer {
         const options = config.subtitles_new.map((item) => ({
             src: item.url,
             srclang: item.locale,
-            kind: "subtitles",
+            kind: 'subtitles',
             label: item.locale_label,
         }));
 
@@ -315,8 +315,8 @@ export default class EmbedPlayer {
     }
 
     isSafari() {
-        let chromeUserAgent = navigator.userAgent.indexOf("Chrome") > -1;
-        let safariUserAgent = navigator.userAgent.indexOf("Chrome") > -1;
+        let chromeUserAgent = navigator.userAgent.indexOf('Chrome') > -1;
+        let safariUserAgent = navigator.userAgent.indexOf('Chrome') > -1;
         if (chromeUserAgent && safariUserAgent) safariUserAgent = false;
         return safariUserAgent;
     }
@@ -326,11 +326,11 @@ export default class EmbedPlayer {
 
         // check if the entitlements contain FPS in order to know when to filter out aes
         const filterAES = !!config.entitlements.find(
-            (entitlement) => entitlement.encryption_type === "fps"
+            (entitlement) => entitlement.encryption_type === 'fps'
         );
         const entitlements = filterAES
             ? config.entitlements.filter((entitlement) => {
-                return entitlement.encryption_type !== "aes";
+                return entitlement.encryption_type !== 'aes';
             })
             : config.entitlements;
 
@@ -342,22 +342,22 @@ export default class EmbedPlayer {
             };
 
             if (entitlement.token) {
-                if (entitlement.encryption_type === "cenc") {
+                if (entitlement.encryption_type === 'cenc') {
                     entitlementConfig.protectionInfo = [
                         {
-                            type: "PlayReady",
-                            authenticationToken: "Bearer=" + entitlement.token,
+                            type: 'PlayReady',
+                            authenticationToken: 'Bearer=' + entitlement.token,
                         },
                         {
-                            type: "Widevine",
-                            authenticationToken: "Bearer " + entitlement.token,
+                            type: 'Widevine',
+                            authenticationToken: 'Bearer ' + entitlement.token,
                         },
                     ];
-                } else if (entitlement.encryption_type === "fps") {
+                } else if (entitlement.encryption_type === 'fps') {
                     entitlementConfig.protectionInfo = [
                         {
-                            type: "FairPlay",
-                            authenticationToken: "Bearer " + entitlement.token,
+                            type: 'FairPlay',
+                            authenticationToken: 'Bearer ' + entitlement.token,
                             certificateUrl: config.fairplay_certificate_url,
                         },
                     ];
@@ -370,18 +370,18 @@ export default class EmbedPlayer {
 
     setupChromecast(selector, chromecastReceiverAppId) {
         const castButtonContaner = document.querySelector(selector);
-        const castButton = document.createElement("google-cast-launcher");
+        const castButton = document.createElement('google-cast-launcher');
         castButtonContaner.appendChild(castButton);
         if (chromecastReceiverAppId) {
-            window["__onGCastApiAvailable"] = (isAvailable) => {
+            window['__onGCastApiAvailable'] = (isAvailable) => {
                 if (isAvailable && cast && cast.framework) {
                     this.initializeCastApi(chromecastReceiverAppId);
                 }
             };
 
-            const scriptElement = document.createElement("script");
+            const scriptElement = document.createElement('script');
             scriptElement.src =
-                "https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1";
+                'https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1';
             document.head.appendChild(scriptElement);
         }
     }
@@ -398,7 +398,7 @@ export default class EmbedPlayer {
         );
     }
 
-    getCastMediaInfo = (articlePlayConfig) => {
+    getCastMediaInfo(articlePlayConfig) {
         if (
             articlePlayConfig &&
             articlePlayConfig.config &&
@@ -411,19 +411,19 @@ export default class EmbedPlayer {
                     chrome.cast.media.TrackType.TEXT
                 );
                 castTrack.trackContentId = option.src;
-                castTrack.trackContentType = "text/vtt";
+                castTrack.trackContentType = 'text/vtt';
                 castTrack.subtype = chrome.cast.media.TextTrackType.SUBTITLES;
                 castTrack.name = option.label;
                 castTrack.language = option.srclang;
                 castTrack.customDate = null;
                 return castTrack;
             });
-            const contentType = "application/vnd.ms-sstr+xml";
+            const contentType = 'application/vnd.ms-sstr+xml';
             const entitlement = articlePlayConfig.config.player.find((item) => {
                 return item.type === contentType;
             });
             const protectionConfig = entitlement.protectionInfo.find((protection) => {
-                return protection.type === "PlayReady";
+                return protection.type === 'PlayReady';
             });
             const token = protectionConfig
                 ? protectionConfig.authenticationToken
@@ -447,9 +447,9 @@ export default class EmbedPlayer {
             return mediaInfo;
         }
         return null;
-    };
+    }
 
-    getLicenseUrlFromSrc = (src, token) => {
+    getLicenseUrlFromSrc (src, token) {
         if (token) {
             const res1 = src.match(
                 /^https:\/\/([^.]+)\.streaming\.mediaservices\.windows\.net\/.*/i
@@ -461,9 +461,9 @@ export default class EmbedPlayer {
 
             if (res && res.length === 2) {
                 const licenseUrl =
-                    "https://" +
+                    'https://' +
                     res[1] +
-                    ".keydelivery.westeurope.media.azure.net/PlayReady/?token=" +
+                    '.keydelivery.westeurope.media.azure.net/PlayReady/?token=' +
                     encodeURIComponent(token);
                 return {
                     licenseUrl,
@@ -472,20 +472,20 @@ export default class EmbedPlayer {
             }
         }
         return {};
-    };
+    }
 
     castVideo({apiBaseUrl, projectId, articleId, assetId, token}) {
         if (!apiBaseUrl) {
-            return Promise.reject("apiBaseUrl property is missing");
+            return Promise.reject('apiBaseUrl property is missing');
         }
         if (!articleId) {
-            return Promise.reject("articleId property is missing");
+            return Promise.reject('articleId property is missing');
         }
         if (!assetId) {
-            return Promise.reject("assetId property is missing");
+            return Promise.reject('assetId property is missing');
         }
         if (!projectId) {
-            return Promise.reject("projectId property is missing");
+            return Promise.reject('projectId property is missing');
         }
         const apiFetchUrl = `${apiBaseUrl}/graphql/${projectId}`;
         return this.getPlayConfig(
@@ -504,7 +504,7 @@ export default class EmbedPlayer {
                     request.currentTime = config.currentTime;
                     return castSession.loadMedia(request);
                 } else {
-                    throw {message: "Unexpected manifest format in articlePlayConfig"};
+                    throw {message: 'Unexpected manifest format in articlePlayConfig'};
                 }
             }
             return config;
--- src/video-cast.js
+++ src/video-cast.js
@@ -1,28 +1,28 @@
-import EmbedPlayer from "./embed-player.js";
+import EmbedPlayer from './embed-player.js';
 
 (function () {
     const urlQueryString = window.location.search;
     const urlParams = new URLSearchParams(urlQueryString);
 
-    const articleId = +urlParams.get("articleId");
-    const projectId = +urlParams.get("projectId");
-    const assetId = +urlParams.get("assetId");
-    const apiBaseUrl = urlParams.get("apiBaseUrl");
-    const token = urlParams.get("token");
-    const posterImageUrl = urlParams.get("posterImageUrl");
-    const autoplay = urlParams.get("autoplay");
-    const chromecastReceiverAppId = urlParams.get("chromecastReceiverAppId");
+    const articleId = +urlParams.get('articleId');
+    const projectId = +urlParams.get('projectId');
+    const assetId = +urlParams.get('assetId');
+    const apiBaseUrl = urlParams.get('apiBaseUrl');
+    const token = urlParams.get('token');
+    const posterImageUrl = urlParams.get('posterImageUrl');
+    const autoplay = urlParams.get('autoplay');
+    const chromecastReceiverAppId = urlParams.get('chromecastReceiverAppId');
 
     const tokenParameter = token ? {token} : {};
     const posterImageUrlParameter = posterImageUrl ? {posterImageUrl} : {};
 
     const player = new EmbedPlayer();
 
-    document.getElementById("video-button-start").addEventListener("click", playVideo);
-    document.getElementById("video-button-stop").addEventListener("click", stopCastVideo);
+    document.getElementById('video-button-start').addEventListener('click', playVideo);
+    document.getElementById('video-button-stop').addEventListener('click', stopCastVideo);
 
-    player.setupChromecast("#cast-wrapper", chromecastReceiverAppId);
-    document.getElementById("cast-wrapper").style.display = "unset";
+    player.setupChromecast('#cast-wrapper', chromecastReceiverAppId);
+    document.getElementById('cast-wrapper').style.display = 'unset';
 
     function playVideo() {
         if (player.isConnected()) {
@@ -38,14 +38,14 @@ import EmbedPlayer from "./embed-player.js";
         } else {
             player
                 .play({
-                    selector: ".video-wrapper",
+                    selector: '.video-wrapper',
                     apiBaseUrl,
                     articleId,
                     projectId,
                     assetId,
                     ...tokenParameter,
                     ...posterImageUrlParameter,
-                    autoplay: autoplay && autoplay === "true",
+                    autoplay: autoplay && autoplay === 'true',
                 })
                 .catch((error) => {
                     console.error(error);
