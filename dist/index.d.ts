a1977a95853c1cf3336678613aa604df77548582
Jeroen Carolus <j.carolus@peoplesplayground.nl>
2025-04-15 11:32:16 +0200

    * expose player-logging service

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,4 +6,6 @@ import { InitParams } from './models/play-params';
 import { PlayConfig, MimeTypeHls, MimeType, MimeTypeDash } from './models/play-config';
 import { toMimeType } from './api/converters';
 import { supportsNativeHLS } from './utils/platform';
-export { EmbedPlayer, VideoPlayer, ChromecastControls, ChromecastSender, InitParams, PlayConfig, supportsNativeHLS, MimeTypeHls, MimeTypeDash, MimeType, toMimeType, };
+import { PlayerLoggerService } from './logging/player-logger-service';
+import { PlayerDeviceTypes } from './models/player';
+export { EmbedPlayer, VideoPlayer, ChromecastControls, ChromecastSender, InitParams, PlayConfig, supportsNativeHLS, MimeTypeHls, MimeTypeDash, MimeType, toMimeType, PlayerLoggerService, PlayerDeviceTypes };
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {InitParams} from './models/play-params';
 import {PlayConfig, MimeTypeHls, MimeType, MimeTypeDash} from './models/play-config';
 import {toMimeType} from './api/converters';
 import {supportsNativeHLS} from './utils/platform';
+import {PlayerLoggerService} from './logging/player-logger-service';
+import {PlayerDeviceTypes} from './models/player';
 
 export {
     EmbedPlayer,
@@ -19,4 +21,6 @@ export {
     MimeTypeDash,
     MimeType,
     toMimeType,
+    PlayerLoggerService,
+    PlayerDeviceTypes,
 };
